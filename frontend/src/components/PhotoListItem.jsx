0dcc02e79204964ea07521b7279355f1a232af13
JT Smith <nexxai@gmail.com>
2023-10-04 13:35:47 -0600

    Adding styling to PhotoListItem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,23 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  return (
+    <article className="photo-list__item">
+      <section>
+        <img src={props.photo.imageSource} className="photo-list__image" />
+      </section>
+      <section>
+        <img src={props.photo.profile} className="photo-list__image" />
+      </section>
+      <section className="photo-list__user-details">
+        <p className="photo-list__user_info">{props.photo.username}</p>
+        <p className="photo-list__user-location">
+          {props.photo.location.city}, {props.photo.location.country}
+        </p>
+      </section>
+    </article>
+  );
 };
 
 export default PhotoListItem;
