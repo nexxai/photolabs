7275532671f223ad8c938f25bdc4f3c7119d0796
JT Smith <nexxai@gmail.com>
2023-10-05 13:38:37 -0600

    Removing sample data and using data from /mocks

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -4,11 +4,14 @@ import "../styles/HomeRoute.scss";
 import TopNavigationBar from "components/TopNavigationBar";
 import PhotoList from "components/PhotoList";
 
+import photos from "mocks/photos";
+import topics from "mocks/topics";
+
 const HomeRoute = () => {
   return (
     <div className="home-route">
-      <TopNavigationBar />
-      <PhotoList />
+      <TopNavigationBar topics={topics} />
+      <PhotoList photos={photos} />
     </div>
   );
 };
