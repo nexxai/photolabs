e9ee21343632ba4dade2c3ae2a964bb4ffa9cb13
JT Smith <nexxai@gmail.com>
2023-10-05 13:28:55 -0600

    Adding TopicList and TopicListItem components

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/TopicList.scss";
+import TopicListItem from "./TopicListItem";
 
 const sampleDataForTopicList = [
   {
@@ -23,7 +24,16 @@ const sampleDataForTopicList = [
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
+      {sampleDataForTopicList.map((topic) => {
+        return (
+          <TopicListItem
+            key={topic.id}
+            id={topic.id}
+            slug={topic.slug}
+            title={topic.title}
+          />
+        );
+      })}
     </div>
   );
 };
