258cee02f37f79fbf173e1f06e5aba415191c226
JT Smith <nexxai@gmail.com>
2023-10-06 16:28:28 -0600

    Integrating design changes

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -25,6 +25,7 @@ function PhotoFavButton(props) {
     <div className="photo-list__fav-icon" onClick={favoritePhoto}>
       <div className="photo-list__fav-icon-svg">
         <FavIcon
+          displayAlert={props.likedPhotos.length ? true : false}
           selected={
             favorited ||
             (props.likedPhotos && props.likedPhotos.includes(props.photo_id))
