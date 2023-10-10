1f286a8f537c12860430d75e144f210f66af76e0
JT Smith <nexxai@gmail.com>
2023-10-10 16:56:13 -0600

    Liking a photo does not add an unnecessary indicator

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,24 +3,14 @@ import "../styles/PhotoFavButton.scss";
 import FavIcon from "./FavIcon";
 
 function PhotoFavButton(props) {
-  const [favorited, setFavorited] = useState(false);
-
   const favoritePhoto = function () {
-    setFavorited(!favorited);
-
     props.setLikedPhotos(props.photo_id);
   };
 
   return (
     <div className="photo-list__fav-icon" onClick={favoritePhoto}>
       <div className="photo-list__fav-icon-svg">
-        <FavIcon
-          displayAlert={props.likedPhotos.length ? true : false}
-          selected={
-            favorited ||
-            (props.likedPhotos && props.likedPhotos.includes(props.photo_id))
-          }
-        />
+        <FavIcon selected={props.likedPhotos.includes(props.photo_id)} />
       </div>
     </div>
   );
